2690be21e9c48effeaff1c3251adf10744e3a145
SrHeisenberg27 <alvaro.ferrandez68@gmail.com>
2020-05-29 15:44:30 +0200

    Initial Commit

---
+++ WIMM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WIMM
+//
+//  Created by Álvaro Ferrández Gómez on 29/05/2020.
+//  Copyright © 2020 Álvaro Ferrández Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
